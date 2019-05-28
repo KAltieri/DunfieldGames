52494085be9c3cc612a9f78291fdc8b89382cdaa
Kevin <kdalt412@gmail.com>
2019-05-27 21:43:11 -0700

    Player Movement + Basic Tiles

---
+++ Game/Assets/Character.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Character : MonoBehaviour
+{
+    public float speed;
+    private CharacterController controller;
+    float horizontalMovement = 0.0f;
+    float verticalMovement = 0.0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        controller = GetComponent<CharacterController>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        horizontalMovement = Input.GetAxisRaw("Horizontal");
+        verticalMovement = Input.GetAxisRaw("Vertical");
+    }
+
+    void FixedUpdate()
+    {
+        Vector3 move = new Vector3(horizontalMovement, verticalMovement, 0.0f);
+        Debug.Log(move);
+        move = transform.TransformDirection(move);
+        move *= speed;
+
+        controller.Move(move * Time.deltaTime);
+    }
+}
